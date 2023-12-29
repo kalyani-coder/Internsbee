f99090031f6cac2e35f6c99c3ca2a14a373ad698
Vedant0370 <vedant.0370@gmail.com>
2023-12-29 16:14:41 +0530

    update app.js, StudentsDetails.js and StudentsDetails.js

--- backend/models/StudentsDetails.js
+++ backend/models/StudentsDetails.js
@@ -1,40 +1,38 @@
 const mongoose = require('mongoose');
 
 const StudentDetailsSchema = new mongoose.Schema({
-  personalDetails: {
+ 
+  student_id : String,
     firstName: String,
     lastName: String,
     email: String,
     birthdate: String,
-    permanentAddress: {
+   
       city: String,
       district: String,
       country: String,
-    },
-    currentAddress: {
+   
+   
       city: String,
       district: String,
       country: String,
-    },
-  },
-  educationalDetails: {
+ 
+  
     education: String,
     instituteName: String,
     stream: String,
     passOutYear: String,
-  },
-  technicalDetails: {
+
     keySkills: String,
     languages: String,
     experience: String,
     salaryExpectations: String,
     projectName: String,
     projectSummary: String,
-  },
-  uploadDocuments: {
+
     resume: String,
     certificate: String,
-  },
+ 
 });
 
 const StudentDetailsModel = mongoose.model('StudentDetails', StudentDetailsSchema);
