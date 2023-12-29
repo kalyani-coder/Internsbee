1888e544f048ff09fe3ac70f344b225637c9db91
amardip padghan <82278378+amardippadghan@users.noreply.github.com>
2023-12-29 16:24:41 +0530

    adding route to student details

--- backend/routes/StudentsDetails.js
+++ backend/routes/StudentsDetails.js
@@ -1,7 +1,91 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const StudentDetailsModel = require('../models/StudentsDetails'); // Adjust the path accordingly
+const StudentDetailsModel = require("../models/StudentsDetails"); // Adjust the path accordingly
 
-// Get route 
+// GET route
+router.get("/", async (req, res) => {
+  try {
+    const students = await StudentDetailsModel.find();
+    res.json(students);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const student = await StudentDetailsModel.findById(id);
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+    res.json(student);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+router.get("/studentId/:id", async (req, res) => {
+  const studentId = req.params.id;
+  try {
+    const student = await StudentDetailsModel.findOne({
+      student_id: studentId,
+    });
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+    res.json(student);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// POST route
+router.post("/", async (req, res) => {
+  const studentData = req.body;
+  try {
+    const student = new StudentDetailsModel(studentData);
+    await student.save();
+    res.status(201).json(student);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// PATCH route
+router.patch("/:id", async (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+
+  try {
+    const student = await StudentDetailsModel.findByIdAndUpdate(id, updates, {
+      new: true,
+    });
+
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    res.json(student);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// DELETE route
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedStudent = await StudentDetailsModel.findByIdAndDelete(id);
+
+    if (!deletedStudent) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    res.json({ message: "Student deleted successfully", deletedStudent });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 module.exports = router;
